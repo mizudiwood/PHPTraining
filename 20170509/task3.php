6f1c5b38dac5e7f65f59e2cd1afa8831bde914e8
mizudiwood <mizudiwood@gmail.com>
2017-05-09 17:18:39 +0900

    0509-6
    Signed-off-by: mizudiwood <mizudiwood@gmail.com>

--- 20170509/task3.php
+++ 20170509/task3.php
@@ -14,10 +14,10 @@
     $x = 1;
 
     for($i=0;$i<5;$i++){
-            echo "number　: $x <br>";
+            echo "number　: {$x} <br />";
             $x++;
     }
-   echo "<br>";
+   echo "<br />";
 /*    $colors = array("赤", "緑", "青", "黄色");
 
     foreach ($colors as $value) {
@@ -28,8 +28,8 @@
 */
     $colors = ["赤","緑","青","黄色"];
 
-    for($i=0;$i<4;$i++){
+    for($i=0;$i<count($colors);$i++){
 
-            echo "$colors[$i] <br />";
+            echo "{$colors[$i]} <br />";
     }
 ?>
