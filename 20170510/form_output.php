9a9c54861f4e43523b9024b6af5e7ceb48c3d411
mizudiwood <mizudiwood@gmail.com>
2017-05-11 11:56:31 +0900

    0511-02

--- 20170510/form_output.php
+++ 20170510/form_output.php
@@ -1,17 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="task.css" type="text/css" />
+</style>
+</head>
+
 <?php
 echo "<h3>&nbsp;&nbsp;&nbsp;&nbsp;お問い合わせ</h3>" ;
-echo "・" . "名前：" . $_POST['name1'] . "&nbsp" .$_POST['name2'];
+echo "・" . "<b>名前：</b>" . $_POST['name1'] . "&nbsp" .$_POST['name2'];
 echo "<br />" ;
-echo "・" . "性别：" . $_POST['radiosex'];
+echo "・" . "<b>性别：</b>" . $_POST['radiosex'];
 echo "<br />";
-echo "・" . "住所：" . $_POST['add'];
+echo "・" . "<b>住所：</b>" . $_POST['add'];
 echo "<br />";
-echo "・" . "電話番号：" . $_POST['tel1'],"-",$_POST['tel2'],"-",$_POST['tel3'];
+echo "・" . "<b>電話番号：</b>" . $_POST['tel1'],"-",$_POST['tel2'],"-",$_POST['tel3'];
 echo "<br />";
-echo "・" . "メールアドレス：" . $_POST['mail'];
+echo "・" . "<b>メールアドレス：</b>" . $_POST['mail'];
 echo "<br />";
 
-echo "・" . "どこで知ったか：";
+echo "・" . "<b>どこで知った：</b>";
 $checkbox = $_POST['checkbox'];
 
 for($i=0;$i<count($checkbox);$i++){
@@ -22,10 +30,11 @@ for($i=0;$i<count($checkbox);$i++){
         }
 }
 echo "<br />";
-echo "・" . "質問カテゴリ：" . $_POST['ques'];
+echo "・" . "<b>質問カテゴリ：</b>" . $_POST['ques'];
 echo "<br />";
-echo "・" . "質問内容：" . "<br />" . "<div>"  . $_POST['content']  . "</div>";
+echo "・" . "<b>質問内容：</b>" . "<br />" . "<div>"  . $_POST['content']  . "</div>";
 
 ?>
 <br />
 <br /><input type="button" name="Submit" onclick="javascript:history.back(-1);" value="未入力状態でお書き直します">
+</html>
