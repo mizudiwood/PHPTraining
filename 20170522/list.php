cf05eb507a61c9bba105f27ad58b18d4d2054e30
mizudiwood <mizudiwood@gmail.com>
2017-05-23 12:22:08 +0900

    0523-4

--- 20170522/list.php
+++ 20170522/list.php
@@ -29,8 +29,74 @@ background: #CF7A7A
 </head>
 
 <body>
-    <div class="bb">
+    <div class="dd">
      <h3>お問い合わせ一覧</h3>
+
+
+     <table border="1">
+  <tr>
+    <th>No.</th>
+    <th>氏名</th>
+    <th>性別</th>
+    <th>住所</th>
+    <th>電話番号</th>
+    <th>メールアドレス</th>
+    <th>どこで知ったか</th>
+    <th>質問カテゴリ</th>
+    <th>質問内容</th>
+  </tr>
+
+  <?php
+  $csvfile = "question.csv";
+  $counterfile = "counter.txt";
+
+
+  $fp = fopen( "counter.txt", "r+" ); // ファイル開く
+  $myfile = fopen( "question.csv", "r" );
+
+  $total = fgets( $fp, 100000 ); // 9桁分値読み取り
+  $number = intval($total);
+
+  fclose( $fp );
+
+  for($i=0;$i<$number;$i++){
+
+//      define('STDIN',fopen("question.csv","r"));
+
+          $s1 = trim(fgets($myfile));
+          $arr = explode(" ", $s1);
+echo "<tr>";
+echo "<td>";
+$no = $i +1;
+echo "$no";
+echo "</td>";
+
+echo "<td>";
+echo "$arr[0]" . " " . "$arr[1]";
+echo "</td>";
+
+for($j=2;$j<4;$j++){
+
+   echo "<td>";
+   echo "$arr[$j]";
+   echo "</td>";
+}
+
+echo "<td>";
+echo "$arr[4]" . "-" . "$arr[5]" . "-" . "$arr[6]";
+echo "</td>";
+
+echo "<td>";
+echo "$arr[7]";
+echo "</td>";
+
+   echo "</tr>";
+  }
+
+
+
+  ?>
+</table>
  </div>
 </body>
 
