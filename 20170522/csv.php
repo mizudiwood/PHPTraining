539e57d43fa1a54a6154c95ddf74200ebbfa1ae2
mizudiwood <mizudiwood@gmail.com>
2017-05-23 10:44:24 +0900

    0523-2

--- 20170522/csv.php
+++ 20170522/csv.php
@@ -1,9 +1,25 @@
 <?php
-            $counterfile = "question.csv";
+            $csvfile = "question.csv";
+            $counterfile = "counter.txt";
 
-            if (file_exists($counterfile) )
+           if (file_exists($counterfile) ){
+            $fp = fopen( "counter.txt", "r+" ); // ファイル開く
+
+            $total = fgets( $fp, 100000 ); // 9桁分値読み取り
+            $number = intval($total);
+
+            fclose( $fp );
+
+            }
+
+            if (file_exists($csvfile) )
             {
             $myfile = fopen( "question.csv", "a" ); // ファイル開く
+
+
+            $number= $number +1; // 値+1（カウントアップ）
+
+
             fwrite($myfile, $_GET['name1'] . " " . $_GET['name2']);
             fwrite($myfile, " " . $_GET['radiosex'] . " " . $_GET['add']);
             fwrite($myfile, " " . $_GET['tel1'] . " " . $_GET['tel2'] . " " . $_GET['tel3']);
@@ -19,30 +35,38 @@
 
                $myfile = fopen("question.csv", "w");
 
+               $number = 1;
+    //           fwrite($myfile, $number . "\r\n");
+
                fwrite($myfile, $_GET['name1'] . " " . $_GET['name2']);
                fwrite($myfile, " " . $_GET['radiosex'] . " " . $_GET['add']);
                fwrite($myfile, " " . $_GET['tel1'] . " " . $_GET['tel2'] . " " . $_GET['tel3']);
                fwrite($myfile, " " . $_GET['mail']);
 
-               if(isset($_GET['checkbox'])){
-                   $checkbox = $_GET['checkbox'];
-                   $count = count($checkbox);
-                   fwrite($myfile, " " . $count);
+               fwrite($myfile, " " . $_GET['a']);
 
-                   for($i=0;$i<$count;$i++){
-
-                           fwrite($myfile, " " . $checkbox[$i]);
-                   }
-               }
-                   else {
-                      $count = 0;
-                      fwrite($myfile, " " . 0);
-                   }
-
-               fwrite($myfile, " " . $_GET['ques'] . " " . $_GET['content']. "\r\n");
+               fwrite($myfile, " " . $_GET['ques'] . " " . $_GET['content'] . "\r\n");
                fclose($myfile);
             }
 
-        echo "お問い合わせありがとうございました！" . "<br>";
-        echo "お問い合わせ番号:" . "";
+
+            if (file_exists($counterfile) )
+            {
+           $fp = fopen( "counter.txt", "r+" ); // ファイル開く
+
+           rewind( $fp ); // ファイルポインタを先頭に戻す
+           fputs( $fp, $number ); // 値書き込み
+           fclose( $fp ); // ファイル閉じる
+           }
+
+           else {
+               $myfile = fopen("counter.txt", "w");
+
+               fwrite($myfile, $number);
+               fclose($myfile);
+           }
+
+
+            echo "お問い合わせありがとうございました！" . "<br>";
+            echo "お問い合わせ番号:" . "$number";
 ?>
