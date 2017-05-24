e6e6d7ecdbc1172a184485d2bdd9c1ce8977fc5b
mizudiwood <mizudiwood@gmail.com>
2017-05-24 09:38:13 +0900

    0524-1

--- 20170522/list.php
+++ 20170522/list.php
@@ -32,25 +32,26 @@ background: #CF7A7A
     <div class="dd">
      <h3>お問い合わせ一覧</h3>
 
-
-     <table border="1">
+     <table border="1" align="center">
   <tr>
-    <th>No.</th>
-    <th>氏名</th>
-    <th>性別</th>
-    <th>住所</th>
-    <th>電話番号</th>
-    <th>メールアドレス</th>
-    <th>どこで知ったか</th>
-    <th>質問カテゴリ</th>
-    <th>質問内容</th>
+    <th width="20px">No.</th>
+    <th width="100px">氏名</th>
+    <th width="100px">性別</th>
+    <th width="100px">住所</th>
+    <th width="100px">電話番号</th>
+    <th width="100px">メールアドレス</th>
+    <th width="100px">どこで知ったか</th>
+    <th width="100px">質問カテゴリ</th>
+    <th width="100px">質問内容</th>
   </tr>
 
   <?php
+
+
   $csvfile = "question.csv";
   $counterfile = "counter.txt";
 
-
+ if (file_exists($counterfile)){
   $fp = fopen( "counter.txt", "r+" ); // ファイル開く
   $myfile = fopen( "question.csv", "r" );
 
@@ -65,36 +66,62 @@ background: #CF7A7A
 
           $s1 = trim(fgets($myfile));
           $arr = explode(" ", $s1);
+
+//ID番号
 echo "<tr>";
 echo "<td>";
 $no = $i +1;
 echo "$no";
 echo "</td>";
-
+//氏名
 echo "<td>";
 echo "$arr[0]" . " " . "$arr[1]";
 echo "</td>";
-
+//性別、住所
 for($j=2;$j<4;$j++){
 
    echo "<td>";
    echo "$arr[$j]";
    echo "</td>";
 }
-
+//電話番号
 echo "<td>";
 echo "$arr[4]" . "-" . "$arr[5]" . "-" . "$arr[6]";
 echo "</td>";
-
+//メールアドレス
 echo "<td>";
 echo "$arr[7]";
 echo "</td>";
+//どこで知ったか
+$where = $arr[8];
+if($where==0){
+    echo "<td>";
+    echo "</td>";
+}
+else{
+    echo "<td>";
+    for($j=9;$j<$where+9;$j++){
 
+        echo "$arr[$j]";
+        if ($j<$arr[8]+8) {
+            echo "、";
+        }
+    }
+    echo "</td>";
+}
+//質問カテゴリ
+$q=$where + 9;
+echo "<td>";
+echo "$arr[$q]";
+echo "</td>";
+//質問内容
+$qw=$where + 10;
+echo "<td>";
+echo "$arr[$qw]";
+echo "</td>";
    echo "</tr>";
   }
-
-
-
+}
   ?>
 </table>
  </div>
